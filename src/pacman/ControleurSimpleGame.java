ae9f93a3f1171cf93472f669de57e032c9f6d670
Matthew Coyle <matthew@coyle.fr>
2020-10-13 15:09:05 +0200

    start of new class

---
+++ src/pacman/ControleurSimpleGame.java
@@ -0,0 +1,35 @@
+package pacman;
+
+public class ControleurSimpleGame implements InterfaceControleur {
+
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void step() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setTime(double time) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
