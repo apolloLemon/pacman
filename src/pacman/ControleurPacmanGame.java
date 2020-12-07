35c1afc8c40cb3921d52d13a40d3d431ba754e11
Matthew Coyle <matthew@coyle.fr>
2020-12-07 16:52:24 +0100

    implemented interface

--- src/pacman/ControleurPacmanGame.java
+++ src/pacman/ControleurPacmanGame.java
@@ -21,32 +21,46 @@ public class ControleurPacmanGame implements InterfaceControleur {
 	
 	@Override
 	public void start() {
-		// TODO Auto-generated method stub
+		System.out.println(this.getClass().getName()+": start");
+		vc.runButton.setEnabled(true);
+		vc.stepButton.setEnabled(true);
+		g.init();
 
 	}
 
 	@Override
 	public void step() {
+		System.out.println(this.getClass().getName()+": step");
 		// TODO Auto-generated method stub
-
+		g.step();
 	}
 
 	@Override
 	public void run() {
+		System.out.println(this.getClass().getName()+": run");
+		vc.stepButton.setEnabled(false);
+		vc.runButton.setEnabled(false);
+		vc.pauseButton.setEnabled(true);
 		// TODO Auto-generated method stub
-
+		g.launch();
+		
 	}
 
 	@Override
 	public void pause() {
+		System.out.println(this.getClass().getName()+": pause");
+		vc.stepButton.setEnabled(true);
+		vc.runButton.setEnabled(true);
 		// TODO Auto-generated method stub
-
+		g.pause();
+		
 	}
 
 	@Override
 	public void setTime(double time) {
+		System.out.println(this.getClass().getName()+": setTime = "+time);
 		// TODO Auto-generated method stub
-
+		g.setTurnspersec((long) time);
 	}
 
 }
