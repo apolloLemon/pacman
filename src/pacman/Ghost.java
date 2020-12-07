f6ad5cf9555018cf315427627c0431cb178e08a0
Matthew Coyle <matthew@coyle.fr>
2020-12-07 23:02:50 +0100

    added extra strategy fields in Ghost to store it's varied strats

--- src/pacman/Ghost.java
+++ src/pacman/Ghost.java
@@ -3,10 +3,14 @@ package pacman;
 public class Ghost extends PacmanAgent {
 
 	Boolean scared;
+	PacmanStrategy flee;
+	PacmanStrategy hunt;
 	
 	Ghost(PositionAgent xy) {
 		super(xy);
 		scared = false;
+		flee = new RandomStrat();
+		hunt = new ValidRandomMoveStrat(this);
 	}
 	
 }
