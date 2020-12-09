136dc2d34255bd01d7e1c92e166ab191ad5c941f
Matthew Coyle <matthew@coyle.fr>
2020-12-09 11:28:07 +0100

    AStarStrat init

---
+++ src/pacman/AStarStrat.java
@@ -0,0 +1,26 @@
+package pacman;
+
+public class AStarStrat implements PacmanStrategy {
+
+	PacmanAgent body;
+	PositionAgent target;
+	
+	boolean seekTarget;
+	
+	public AStarStrat (PacmanAgent body) {
+		this.body=body;
+		seekTarget=true; //must find a living pacman to go to
+	}
+	public AStarStrat (PacmanAgent body, PositionAgent target) {
+		this.body=body;
+		this.target=target;
+		seekTarget=false; //if you only need to go one place
+	}
+	
+	
+	@Override
+	public AgentAction action(PacmanGame m) {
+		return null;
+	}
+
+}
