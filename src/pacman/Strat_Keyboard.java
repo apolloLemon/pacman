2ad2d211a9d1ff3d8c461d38d094c6a7c5f85891
Matthew Coyle <matthew@coyle.fr>
2020-12-18 13:01:27 +0100

    class for Strat_Keyboard

---
+++ src/pacman/Strat_Keyboard.java
@@ -0,0 +1,16 @@
+package pacman;
+
+public class Strat_Keyboard implements PacmanStrategy {
+
+	PacmanAgent body;
+	int controller;
+	
+	@Override
+	public AgentAction action(PacmanGame m) {
+		
+		
+		
+		return null;
+	}
+
+}
